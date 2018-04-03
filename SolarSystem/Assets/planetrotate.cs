c620d457a8acff353315b57018048505bbc27476
acheng <31626339+wsc16340223@users.noreply.github.com>
2018-04-03 13:35:42 +0800

    Add files via upload

---
+++ SolarSystem/Assets/planetrotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetrotate : MonoBehaviour {
+
+    public Transform sun;
+    public float speed = 10;
+    float rty, rtz;
+
+	// Use this for initialization
+	void Start () {
+        rty = Random.Range(1, 360);
+        rtz = Random.Range(1, 360);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 axis = new Vector3(0, rty, rtz);
+        this.transform.RotateAround(sun.position, axis, speed * Time.deltaTime);
+	}
+}
